4d9510185800bafbb342305f52a8f03a107eef49
Teena Tom <tomteena7@gmail.com>
2023-05-09 14:10:16 -0700

    successfullt merged feature branch with develop branch

--- controllers/daycareController.js
+++ controllers/daycareController.js
@@ -13,3 +13,20 @@ module.exports.getAll = (_req, res) => {
       res.status(400).send(`Error retrieving Daycares: ${err}`)
     );
 };
+
+//Get  a specific daycare details
+module.exports.getDaycare = (req, res) => {
+  const id = req.params.id;
+  db("daycares")
+    .where({ id })
+    .then((rows) => {
+      if (rows.length > 0) {
+        res.status(200).json(rows[0]); // Return the first row in the response
+      } else {
+        res.status(404).send("Item not found"); // Return a 404 error if no rows are returned
+      }
+    })
+    .catch((error) => {
+      res.status(400).send(`Error retrieving the Daycare: ${err}`);
+    });
+};
