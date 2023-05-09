ea360d7b659ddb13ab06a3df1b075b120e02d56f
Teena Tom <tomteena7@gmail.com>
2023-05-09 14:13:47 -0700

    corrected the bug in pushing from branch fetaure/routes

--- controllers/daycareController.js
+++ controllers/daycareController.js
@@ -9,7 +9,22 @@ module.exports.getAll = (_req, res) => {
     .then((data) => {
       res.status(200).json(data);
     })
-    .catch((err) =>
-      res.status(400).send(`Error retrieving Daycares: ${err}`)
-    );
+    .catch((err) => res.status(400).send(`Error retrieving Daycares: ${err}`));
+};
+
+//Get  a specific daycare details
+module.exports.getDaycare = (req, res) => {
+  const id = req.params.id;
+  db("daycares")
+    .where({ id })
+    .then((rows) => {
+      if (rows.length > 0) {
+        res.status(200).json(rows[0]); // Return the first row in the response
+      } else {
+        res.status(404).send("Item not found"); // Return a 404 error if no rows are returned
+      }
+    })
+    .catch((error) => {
+      res.status(400).send(`Error retrieving the Daycare: ${err}`);
+    });
 };
